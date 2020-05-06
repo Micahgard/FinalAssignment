c18b2253decb6ad999c3bea97a33688c1c2982b8
Micah Gardner <micahgardner7@gmail.com>
2020-05-06 18:09:33 +1200

    new changes

--- class/Student.php
+++ class/Student.php
@@ -7,7 +7,7 @@ class Student
     var $name;
     var $username;
     var $password;
-    var $dbconn = "c0vt48zd4okm7ibn";
+    var $dbconn;
 
     public function __construct($id, $name, $username, $password)
     {
@@ -15,16 +15,18 @@ class Student
         $this->name = $name;
         $this->password = $password;
         $this->username = $username;
-        $this->dbconn = new DB();
+        $this->dbconn = (new DB())->conn;
     }
 
-    public function save($id, $name, $username, $password){
-        if (is_null($this->id)){
-            $query = "insert into student values (null, '$this->name', 'this->username', 'this->password')";
-                echo $query;
+    public function save($id, $name, $username, $password)
+    {
+        if (is_null($this->id)) {
+            $query = "insert into student values (null, '$this->name', '$this->username', '$this->password')";
+            echo $query;
             mysqli_query($this->dbconn, $query);
-        }else{
-        $query = "Update student SET = '$this->name' where id = $this->id";
-        mysqli_query($query);
+        } else {
+            $query = "Update student SET = '$this->name' where id = $this->id";
+            mysqli_query($query);
+        }
     }
 }
